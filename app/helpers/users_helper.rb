96f409aa9a783d000d054a70790167d8bc8a4c63
kosuke <gekkouzann334@gmail.com>
2019-06-14 01:07:30 +0900

    pagenate commit

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,11 @@
 module UsersHelper
 
   # 引数で与えられたユーザーのGravatar画像を返す
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 80 })
     dummy_mail = "pool@example.com"
+    size = options[:size]
     gravatar_id = Digest::MD5::hexdigest(dummy_mail.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
