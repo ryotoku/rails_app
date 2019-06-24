4523452e73c3ef17f85b112d6d40377fdebb8eae
Kosuke <gekkouzann334@gmail.com>
2019-06-24 16:47:57 +0900

    herokubug fix

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -5,15 +5,15 @@ require 'optimist'
 module StaticPagesHelper
 
   DEVELOPER_KEY = Settings.api_key
-  YOUTUBE_API_SERVICE_NAME = 'youtube'
-  YOUTUBE_API_VERSION = 'v3'
+  YOUTUBE_API_SERVICE_NAME = Settings.api_name
+  YOUTUBE_API_VERSION = Settings.api_ver
 
   def get_service
     client = Google::APIClient.new(
       key: DEVELOPER_KEY,
       authorization: nil,
       application_name: $PROGRAM_NAME,
-      application_version: '1.0.0'
+      application_version: Settings.app_ver
     )
     youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
 
